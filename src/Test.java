9c6466ba777ef0b6824a5d1f08b8cee4ea9275f5
fabiomondin <fabiomondin@users.noreply.github.com>
2016-10-17 22:15:44 +0200

    Read file page.csv.
    Read the file with the links, skipping a random number of lines to avoid caching by the server (the request are always different pages).
    Save a certain number of links in the support array.

--- src/Test.java
+++ src/Test.java
@@ -3,11 +3,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-/**
- * @author fmondin
- * @author Riccardo Francescato 
- *
- */
+
 public class Test extends Thread {
 
 	/**
@@ -17,55 +13,59 @@ public class Test extends Thread {
 	public Test (String str){
 		super(str);
 	}
+	
 	public void run() {
-
-		Main.times.add(makeRequest("https://192.168.1.10/wikimirror"));
 		
+		Main.times.add(makeRequest("https://192.168.1.10/wikimirror"));
 	}
+	
 	private long makeRequest(String targetURL){
 		String urlParameters = "";
 		HttpURLConnection connection = null;
 		long startTime = System.currentTimeMillis();
 		long endTime = 0;
-		 try {
-			    //Create connection
-			    URL url = new URL(targetURL);
-			    connection = (HttpURLConnection) url.openConnection();
-			    connection.setRequestMethod("POST");
-			    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+		
+	 	try {
+	    	//Create connection
+		    URL url = new URL(targetURL);
+		    connection = (HttpURLConnection) url.openConnection();
+		    connection.setRequestMethod("POST");
+		    connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
 //			    connection.setRequestProperty("Content-Length", Integer.toString(urlParameters.getBytes().length));
-			    connection.setRequestProperty("Content-Language", "en-US");  
+		    connection.setRequestProperty("Content-Language", "en-US");  
 
-			    connection.setUseCaches(false);
-			    connection.setDoOutput(true);
+		    connection.setUseCaches(false);
+		    connection.setDoOutput(true);
 
-			    //Send request
-			    DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
-			    wr.writeBytes(urlParameters);
-			    wr.close();
+		    //Send request
+		    DataOutputStream wr = new DataOutputStream (connection.getOutputStream());
+		    wr.writeBytes(urlParameters);
+		    wr.close();
 
-			    //Get Response  
-			    InputStream is = connection.getInputStream();
-			    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
-			    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
-			    String line;
-			    while ((line = rd.readLine()) != null) {
-			      response.append(line);
-			      response.append('\r');
-			    }
-			    rd.close();
-			    //System.out.println(response.toString());	//print all response
-			    
-			    endTime = System.currentTimeMillis();
-			    //System.out.println("Request received");
-			  } catch (Exception e) {
-			  } finally {
+		    //Get Response  
+		    InputStream is = connection.getInputStream();
+		    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+		    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+		    String line;
+		    while ((line = rd.readLine()) != null) {
+		    	response.append(line);
+		    	response.append('\r');
+		    }
+		    rd.close();
+		    //System.out.println(response.toString());	//print all response
+		    
+		    endTime = System.currentTimeMillis();
+		    //System.out.println("Request received");
+		  } catch (Exception e) {
+			  e.printStackTrace();
+		  } finally {
 			    if (connection != null) {
 			      connection.disconnect();
 			    }
-			  }
+		  }
 		 return(endTime-startTime);
 	}
+	
 
 }
