2c4a07e7ac319bb935dd3edf78dd21f23dc0fba9
fabio-nicola <fabio-nicola@192.168.1.5>
2016-10-17 22:04:18 +0200

    Read file page.csv.
    Read the file with the links, skipping a random number of lines to avoid
    caching by the server (the request are always different pages).
    Save a certain number of links in the support array.

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,74 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Main {
+	
 	public static ArrayList<Long> times;
-	public static void main(String[] args) throws InterruptedException {
-		Long summ = new Long(0);
-		times=new ArrayList();
-		ArrayList<Test> threads=new ArrayList();
-		long startTime = System.currentTimeMillis();
-		for (int n=0; n<10; n++) {
-            threads.add(n, new Test(""+n));
-            threads.get(n).start();
-        }
-		for (Test test : threads) {
-			test.join();
+	public static ArrayList<String> links;
+	
+	public static void main(String[] args) throws InterruptedException, IOException {
+		
+		
+		times = new ArrayList<Long>();
+		links = new ArrayList<String>();
+		
+		//Read file with links
+		int numTotLines = 16814304;
+		int numLink = 100;
+		BufferedReader fileBuffer = null;
+		
+		try {
+			String filePath = "..\\page.csv";
+			fileBuffer = new BufferedReader(new FileReader(filePath));
+			
+			//Skip a random number of line (to avoid caching problems)
+			Random randGen = new Random();
+			int skipLine = randGen.nextInt(numTotLines/2);
+			System.out.println("skipLine="+skipLine);
+			int i = 0;
+			while(fileBuffer.readLine()!=null &&  i<skipLine){
+				i++;
+			}
+			
+			//save the predefined (numLink) number of link
+			i = 0;
+			String nextLine;
+			while((nextLine = fileBuffer.readLine())!=null &&  i<numLink){
+				links.add(nextLine);
+				i++;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(fileBuffer != null){
+				fileBuffer.close();
+			}
 		}
-		for (Long i : times) {
-			summ+=i;
-		}
-		System.out.println("AVG: "+ ((summ/times.size())/1000)+","+((summ/times.size())%1000));
-		System.out.println("Test time: "+ ((System.currentTimeMillis()-startTime)/1000)+","+((System.currentTimeMillis()-startTime)%1000));
+		
+		
+//		ArrayList<Test> threads = new ArrayList();
+//		
+//		long startTime = System.currentTimeMillis();
+//		
+//		for (int n=0; n<2; n++) {
+//            threads.add(n, new Test(""+n));
+//            threads.get(n).start();
+//        }
+//		
+//		for (Test test : threads) {
+//			test.join();
+//		}
+//		
+//		//calculate the total time
+//		Long summ = new Long(0);
+//		for (Long i : times) {
+//			summ+=i;
+//		}
+//		System.out.println("AVG: "+ ((summ/times.size())/1000)+","+((summ/times.size())%1000));		//times is converted in seconds
+//		System.out.println("Test time: "+ ((System.currentTimeMillis()-startTime)/1000)+","+((System.currentTimeMillis()-startTime)%1000));
 		
 	}
 	
